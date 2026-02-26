fb99925b5993c9dbaea1ac2ee6015b176dd3c8b2
Crown battle <dehariyadevansh969@gmail.com>
2026-02-26 10:51:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ from concurrent.futures import ThreadPoolExecutor, TimeoutError
 BOT_TOKEN = os.environ.get('BOT_TOKEN', "8756118508:AAGA06F7DfF9A0T_bYQ1ymoGfNRe_UfGgNY")
 
 # ===== APNI API CONFIG =====
-API_BASE_URL = "https://shivam-ultra-api.onrender.com"
-API_KEY = "SHIVAM-786"
+API_BASE_URL = "https://apple-apixdev.onrender.com/api"
+API_KEY = "DevKing77"
 
 # GROUP & CHANNEL
 GROUP_LINK = "https://t.me/+3I9rJfeh5XVkZDM1"
