6a6c9621d0daea25b29f5421f5faf3d38eb94787
Crown battle <dehariyadevansh969@gmail.com>
2026-03-01 22:39:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor, TimeoutError
 
 # ==================== CONFIGURATION ====================
-BOT_TOKEN = os.environ.get('BOT_TOKEN', "8756118508:AAGA06F7DfF9A0T_bYQ1ymoGfNRe_UfGgNY")
+BOT_TOKEN = os.environ.get('BOT_TOKEN', "8725679605:AAEODqV2bxuSv5pVNMO7prDdGOpjLmcNLrg")
 
 # ===== APNI API CONFIG =====
 API_BASE_URL = "https://apple-apixdev.onrender.com/api"
